60c4552765aab778293d43e851826a868cdf8497
KietLQ <kietlqce182236@fpt.edu.vn>
2025-08-28 13:30:25 +0700

    AddAdminHome

---
+++ src/java/Controller/Admin_Controller/AdminHome.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller.Admin_Controller;
+
+import DAO.DAO_Admin.DAOAdmin;
+import Model.Admin;
+import Model.Orders;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ *
+ * @author lenovo
+ */
+@WebServlet(name = "AdminHome", urlPatterns = {"/AdminHome"})
+public class AdminHome extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = request.getWriter()) {
+            HttpSession session = request.getSession();
+            Admin admin = (Admin) session.getAttribute("admin");
+            if (admin != null) {
+                DAOAdmin daoa= new DAOAdmin();
+                int totalUsers = daoa.GetTotalUsers();
+                int totalOrders = daoa.GetTotalOrder();
+                int totalProducts = daoa.GetTotalProducts();
+                List<Orders> orderList = daoa.GetOrderList();
+                request.setAttribute("totalUsers", totalUsers);
+                request.setAttribute("totalOrders", totalOrders);
+                request.setAttribute("totalProducts", totalProducts);
+                request.setAttribute("orderList", orderList);
+                request.getRequestDispatcher("./View/AdminPage/AdminHome.jsp").forward(request, response);
+            } else {
+                response.sendRedirect("ErrorPage.jsp");
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
